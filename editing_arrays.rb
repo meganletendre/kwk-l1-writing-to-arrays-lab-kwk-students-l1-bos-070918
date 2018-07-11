c61effcca876e5545ad6b30e4b80fefd4f284b33
meganletendre <megan.letendre@comcast.net>
2018-07-11 14:55:20 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -12,5 +12,5 @@ def change_rainbow_colors
 end
 
 def add_colors
-  # Write a solution that adds "green", then "blue" to the rainbow_colors array, then returns the array
+  RAINBOW_COLORS(1, "orange")
 end
