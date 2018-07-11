04210d0a7f39422a510e39492b98299a9f357a55
meganletendre <megan.letendre@comcast.net>
2018-07-11 14:47:41 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -4,10 +4,10 @@ RAINBOW_COLORS = [
   "light_cyan"
 ]
 
-def change_rainbow_colors
+def RAINBOW_COLORS
   RAINBOW_COLORS[0] = red
   RAINBOW_COLORS[1] = light_red
-  RAINBOW_COLORS[2] = light-yellow
+  RAINBOW_COLORS[2] = light_yellow
 end
 
 def add_colors
